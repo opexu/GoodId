85447d4c145ce96b5e8127749bed51fdebda95bb
tgdiop <dev_dev@ro.ru>
2023-07-20 00:48:58 +0300

    types

--- contracts/contracts/idNftMarket.sol
+++ contracts/contracts/idNftMarket.sol
@@ -3,8 +3,6 @@ pragma solidity 0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/common/ERC2981.sol";
 
 contract idNftMarket {
